608ad2ab8721eee1d67d13adcae832a0455d9aa9
Razo <razopetrosyan505349@gmail.com>
2024-05-26 19:45:43 +0400

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Header from '../components/HomeComponent/Header';
 import CartHeader from '../components/HomeComponent/CartHeader';
 import Cart from '../components/HomeComponent/Cart';
 import Carousel from '../components/HomeComponent/Carousel';
+import Wrapper from '../components/commons/Wrapper';
 
 function Home() {
   return (
-<<<<<<< HEAD
-    <div className="cinema__home">
-      <Header />
+    <Wrapper className="cinema__home">
       <Carousel />
       <div className="cinema__home__latest">
         <div className="container">
@@ -20,37 +18,7 @@ function Home() {
           </div>
         </div>
       </div>
-
-      <div className="cinema__home__latest">
-        <div className="container">
-          <CartHeader name="Coming soon" />
-          <div className="cinema__home__latest__row">
-            <Cart />
-            <Cart />
-            <Cart />
-          </div>
-        </div>
-      </div>
-
-      <div className="cinema__home__latest">
-        <div className="container">
-          <CartHeader name="Featured movies" />
-          <div className="cinema__home__latest__row">
-            <Cart />
-            <Cart />
-            <Cart />
-          </div>
-        </div>
-      </div>
-=======
-    <div>
-      <p>45</p>
-      <p>45</p>
-      <p>45</p>
-      <p>46</p>
-      <div>Hello World bzxhsggqxjgi</div>
->>>>>>> 40f1f8a1da973610ad72e962d18d0d57512ffea4
-    </div>
+    </Wrapper>
   );
 }
 
