81581260756926827b91ce5c1e81260492564656
Hrant <avetisyanhrant2014@mail.ru>
2024-05-27 16:03:41 +0400

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import Header from '../components/commons/Header';
 import CartHeader from '../components/HomeComponent/CartHeader';
 import Cart from '../components/HomeComponent/Cart';
 import Carousel from '../components/HomeComponent/Carousel';
+import Wrapper from '../components/commons/Wrapper';
 
 function Home() {
   return (
-    <div className="cinema__home">
-      <Header />
-      <Carousel />
-      <div className="cinema__home__latest">
-        <div className="container">
-          <CartHeader name="Latest" />
-          <div className="cinema__home__latest__row">
-            <Cart />
-            <Cart />
-            <Cart />
+    <Wrapper>
+      <div className="cinema__home">
+        <Carousel />
+        <div className="cinema__home__latest">
+          <div className="container">
+            <CartHeader name="Latest" />
+            <div className="cinema__home__latest__row">
+              <Cart />
+              <Cart />
+              <Cart />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="cinema__home__latest">
-        <div className="container">
-          <CartHeader name="Coming soon" />
-          <div className="cinema__home__latest__row">
-            <Cart />
-            <Cart />
-            <Cart />
+        <div className="cinema__home__latest">
+          <div className="container">
+            <CartHeader name="Coming soon" />
+            <div className="cinema__home__latest__row">
+              <Cart />
+              <Cart />
+              <Cart />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="cinema__home__latest">
-        <div className="container">
-          <CartHeader name="Featured movies" />
-          <div className="cinema__home__latest__row">
-            <Cart />
-            <Cart />
-            <Cart />
+        <div className="cinema__home__latest">
+          <div className="container">
+            <CartHeader name="Featured movies" />
+            <div className="cinema__home__latest__row">
+              <Cart />
+              <Cart />
+              <Cart />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Wrapper>
   );
 }
 
